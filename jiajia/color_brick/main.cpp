2904484fd24a15e17c2e582b2e2250a39f1a4a58
jiajia0516 <871891996@qq.com>
2019-02-25 18:44:08 +0800

    大砖头

--- jiajia/color_brick/main.cpp
+++ jiajia/color_brick/main.cpp
@@ -51,52 +51,51 @@ int main()
                 Point2f rectPoints[4];
                 minRects[i].points(rectPoints);
 
-                double x0= rectPoints[0].x;
-                double y0= rectPoints[0].y;
-                double x1= rectPoints[1].x;
-                double y1= rectPoints[1].y;
-                double x2= rectPoints[2].x;
-                double y2= rectPoints[2].y;
-                double x3= rectPoints[3].x;
-                double y3= rectPoints[3].y;                //定义返回的四个坐标点               
+                double x0 = rectPoints[0].x;
+                double y0 = rectPoints[0].y;
+                double x1 = rectPoints[1].x;
+                double y1 = rectPoints[1].y;
+                double x2 = rectPoints[2].x;
+                double y2 = rectPoints[2].y;
+                double x3 = rectPoints[3].x;
+                double y3 = rectPoints[3].y;                //定义返回的四个坐标点
 
-                double width= sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-                double height= sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1));    //定义长宽
+                double width = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+                double height = sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1));    //定义长宽
                 double area = width * height;                            //面积
-                double w=0,h=0;
-                if(width>height)
+                double w = 0,h = 0;
+                if(width > height)
                 {
-                    h=width;
-                    w=height;
+                    h = width;
+                    w = height;
                 }
                 else
                 {
-                    h=height;
-                    w=width;
+                    h = height;
+                    w = width;
                 }
-                double i = fabs(h/w) ;                     // 高宽比
+                double h_w_ratio = fabs( h / w ) ;                     // 高宽比
 
                 double rect_x1 = (x0 + x2)/2;                 //轮廓在X轴上的中点
                 double rect_y1 = (y0 + y2)/2;                 //轮廓在Y轴上的中点
 
-                Point p0 = Point(rect_x1, rect_y1);
-                double distance_1 = sqrt((rect_x1 -320)*(rect_x1 -320));  //轮廓矩中点到中线的距离
-                double distance_2 = sqrt((rect_x1 - x0)*(rect_x1 - x0)+(rect_y1 - y0)*(rect_y1 - y0));
-                                                                         //角点到轮廓矩中点的距离
-                double xx= rect_x1 - 320;       //判断砖头在中线哪侧 xx>0为右侧，xx<0为左侧
+                Point contour_center = Point(rect_x1, rect_y1);
+                double CENTER_POINT_YELLOWLINE_DISTANCE = sqrt((rect_x1 -320)*(rect_x1 -320));  //轮廓矩中点到中线的距离
+                double ANGLE_CENTER_POINT_DISTANCE = sqrt((rect_x1 - x0)*(rect_x1 - x0)+(rect_y1 - y0)*(rect_y1 - y0));
+                                                                         //轮廓矩形角点到轮廓矩中点的距离
+                double position = rect_x1 - 320;       //判断砖头在中线的哪个方位 position>0为右侧，position<0为左侧
 
-                if(i >2.800 && i< 3.800 && area >1000 )
+                if(h_w_ratio >2.800 && h_w_ratio< 3.800 && area >1000 )
                 {
                      for (int j = 0; j < 4; j++)
                     {
-                        if(distance_1 <distance_2 * 3.5 && xx+15 >0)
+                        if(CENTER_POINT_YELLOWLINE_DISTANCE <ANGLE_CENTER_POINT_DISTANCE * 3.5 && position+15 >0)
                        {
                             line(frame, rectPoints[j], rectPoints[(j + 1) % 4], Scalar(0,255,0), 4, 8, 0);//--
-                            circle(frame, p0,15,Scalar( 0,255, 0),2,8,0);
-
-                             cout<<"h/w"<<i<<endl;
-                            cout<<p0<<endl<<endl;
-                            port.RMSerialWrite(p0.x,p0.y,2);
+                            circle(frame, contour_center,15,Scalar( 0,255, 0),2,8,0);
+//                            cout<<"h/w"<<i<<endl;
+                            cout<<contour_center<<endl<<endl;
+                            port.RMSerialWrite(contour_center.x,contour_center.y,2);
 
                        }
 
@@ -107,7 +106,7 @@ int main()
             }
         }
         imshow("笑果",frame);
-     imshow("2",bin_img);
+        imshow("2",bin_img);
         char (key)=(char) waitKey(30);
         if (key==27)break;
     }
